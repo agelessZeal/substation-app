a4eec20cca5afdba9e1d7dccc31e9b468097d3b5
严 主成 <35507996+mingming924@users.noreply.github.com>
2019-01-26 00:09:21 +0800

    # data api url added

--- substationServer/config/index.js
+++ substationServer/config/index.js
@@ -1,8 +1,10 @@
 let config = {
     mode: 'local',
     port: 3000,
+    //port: 8081,
     graphProfileAPI: "https://graph.microsoft.com/v1.0/me",
     apiBaseURL:"https://192.168.2.103:8080/substation/v1",
-        ts_mode:'hms',//'utc'
+    //apiBaseURL:"https://locahost:8082/substation/v1",
+    ts_mode:'hms',//'utc'
 };
 module.exports = config;
